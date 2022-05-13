29ea886fc06bc64bac49191a51a734c75d3f7881
Wayne <83718013+greagori@users.noreply.github.com>
2022-05-13 08:23:28 -0400

    Update firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -4,13 +4,13 @@ import "firebase/compat/firestore";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBSiS1R7fQS0kP-ncpkevVS4BenE5c3A4I",
-  authDomain: "todo-app-c000d.firebaseapp.com",
-  projectId: "todo-app-c000d",
-  storageBucket: "todo-app-c000d.appspot.com",
-  messagingSenderId: "812108737283",
-  appId: "1:812108737283:web:a54ff15c76410d37401736",
-  measurementId: "G-XHZRW2NJQ2",
+  apiKey: "*************************************",
+  authDomain: "*************************************",
+  projectId: "*************************************",
+  storageBucket: "*************************************",
+  messagingSenderId: "*************************************",
+  appId: "*************************************",
+  measurementId: "*************************************",
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
